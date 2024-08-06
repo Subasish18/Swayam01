666c1f9df27ff394095fb796e000e761209c179e
Subasish18 <subasish996@gmail.com>
2024-08-06 09:08:10 +0530

    Update import yfinance as yf.py

--- import yfinance as yf.py
+++ import yfinance as yf.py
@@ -10,7 +10,6 @@ st.write("""
 A = datetime.datetime.now()
 G = "GOOGL"
 D = yf.Ticker(G)
-DF = D.history(period = '1d',start = "2010-01-01",end = A)
+DF = D.history(period ="1d",start = "2010-01-01",end = A)
 
 st.line_chart(DF.Volume)
-st.bar_chart(DF.Volume)
